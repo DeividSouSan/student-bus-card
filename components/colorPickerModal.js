31353ee3b447ffb1fe13e3e5a586738a0ad01b64
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-12-03 09:29:08 -0300

    feat: fontColor and imgPreview features added

--- components/colorPickerModal.js
+++ components/colorPickerModal.js
@@ -4,37 +4,62 @@ export default function colorPickerModal() {
     // Modal Container
     const modalContainer = document.createElement('div');
     modalContainer.classList.add('colorPickerContainer');
-    
+
     modalContainer.onclick = (event) => {
         if (event.target.classList.contains('colorPickerContainer')) {
             modalContainer.remove();
         }
     }
 
-    // Modal Itself
+    // Modal 
     const modal = document.createElement('div');
     modal.classList.add('colorPickerModal');
 
     const modalHeader = document.createElement('h1');
     modalHeader.innerText = 'Selecione uma cor';
 
-    const colorPickerInput = document.createElement('input');
-    colorPickerInput.type = 'color';
-    colorPickerInput.value = document.documentElement.style.getPropertyValue('--theme-color');
+    const themeColorPicker = document.createElement('input');
+    themeColorPicker.type = 'color';
+    themeColorPicker.value = document.documentElement.style.getPropertyValue('--theme-color');
 
-    const submitButton = document.createElement('button');
-    submitButton.innerText = 'Selecionar';
+    const fontThemePicker = document.createElement('input');
+    fontThemePicker.type = 'color';
+    fontThemePicker.value = document.documentElement.style.getPropertyValue('--font-color');
 
-    submitButton.onclick = () => { 
+
+    let fetchedURL;
+    const imgPreview = document.createElement('img');
+
+    fetch('https://source.unsplash.com/random/480x300')
+        .then(response => {
+            fetchedURL = response.url;
+            imgPreview.src = fetchedURL;
+        })
+
+
+    const selectButton = document.createElement('button');
+    selectButton.innerText = 'Selecionar';
+    selectButton.type = 'button';
+
+
+    selectButton.onclick = async () => {
+
         const localStorage = new LocalStorageController();
-        localStorage.setValues('themeColor', {"color": colorPickerInput.value});
+        localStorage.setValues('themeColor', { "color": themeColorPicker.value });
+        localStorage.setValues('fontColor', { "color": fontThemePicker.value });
+        localStorage.setValues('imageURL', { "url": fetchedURL });
+
         modalContainer.remove();
         window.location.reload();
     }
+
+    const appends = (element, ...appends) => {
+        appends.forEach(append => {
+            element.appendChild(append);
+        });
+    }
     // Modal Appends
-    modal.appendChild(modalHeader);
-    modal.appendChild(colorPickerInput);
-    modal.appendChild(submitButton);
+    appends(modal, modalHeader, themeColorPicker, fontThemePicker, imgPreview, selectButton);
 
     // Appends
     modalContainer.appendChild(modal);
