50ad7cf2c0aec3c988f833b6ac99a0e77e61c822
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-12-04 08:09:22 -0300

    chore: never nester (from 4 to 3 tabs)

--- components/colorPickerModal.js
+++ components/colorPickerModal.js
@@ -1,55 +1,50 @@
 import LocalStorageController from '../controller/LocalStorageController.js';
 
 export default function colorPickerModal() {
-    const template = document.querySelector('.color-picker-template');
-    const modal = template.content.cloneNode(true);
-    const modalContainer = modal.querySelector('.modal-container');
+	const template = document.querySelector('.color-picker-template');
+	const modal = template.content.cloneNode(true);
+	const modalContainer = modal.querySelector('.modal-container');
 
-    modalContainer.addEventListener('click', (event) => {
-        if (event.target.classList.contains('modal-container')) {
-            modalContainer.remove();
-        }
-    });
+	modalContainer.addEventListener('click', (event) => {
+		if (event.target.classList.contains('modal-container')) {
+			modalContainer.remove();
+		}
+	});
 
-    const localStorage = new LocalStorageController();
+	const localStorage = new LocalStorageController();
 
-    const imagePreview = modal.querySelector('img');
-    imagePreview.src = localStorage.getValues('imageURL').url;
-    const refreshImageButton = modal.querySelector('.refresh-image-button');
+	const imagePreview = modal.querySelector('img');
+	imagePreview.src = localStorage.getValues('imageURL').url;
+	const refreshImageButton = modal.querySelector('.refresh-image-button');
 
-    let fetchedURL;
+	let fetchedURL;
 
-    const changeImage = async () => {
-        refreshImageButton.disabled = true;
-        await fetch('https://source.unsplash.com/random/480x300')
-            .then(response => {
-                fetchedURL = response.url;
-                imagePreview.src = fetchedURL;
-            })
-            .finally(() => {
-                refreshImageButton.disabled = false;
-            })
-    }
+	const changeImage = async () => {
+		refreshImageButton.disabled = true;
+		await fetch('https://source.unsplash.com/random/480x300')
+			.then(response => (fetchedURL = response.url, imagePreview.src = fetchedURL))
+			.finally(() => refreshImageButton.disabled = false);
+	}
 
-    refreshImageButton.addEventListener('click', () => {
-        changeImage();
-    });
+	refreshImageButton.addEventListener('click', () => {
+		changeImage();
+	});
 
-    const detailsColor = modal.querySelector('.color-picker-input');
-    detailsColor.value = document.documentElement.style.getPropertyValue('--details-color');
+	const detailsColor = modal.querySelector('.color-picker-input');
+	detailsColor.value = document.documentElement.style.getPropertyValue('--details-color');
 
-    const fontColor = modal.querySelector('.font-color-input');
-    fontColor.value = document.documentElement.style.getPropertyValue('--font-color');
+	const fontColor = modal.querySelector('.font-color-input');
+	fontColor.value = document.documentElement.style.getPropertyValue('--font-color');
 
-    const selectButton = modal.querySelector('.select-button');
-    selectButton.onclick = async () => {
-        localStorage.setValues('detailsColor', { "color": detailsColor.value });
-        localStorage.setValues('fontColor', { "color": fontColor.value });
-        localStorage.setValues('imageURL', { "url": fetchedURL });
+	const selectButton = modal.querySelector('.select-button');
+	selectButton.onclick = async () => {
+		localStorage.setValues('detailsColor', { "color": detailsColor.value });
+		localStorage.setValues('fontColor', { "color": fontColor.value });
+		localStorage.setValues('imageURL', { "url": fetchedURL });
 
-        modalContainer.remove();
-        window.location.reload();
-    }
+		modalContainer.remove();
+		window.location.reload();
+	}
 
-    return modal;
+	return modal;
 }
